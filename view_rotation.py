ff14c159842de59c93d7e1b5aa0ec413db8ff0c0
felipetimoteo <felipetimoteo@id.uff.br>
2025-04-08 20:14:05 -0300

    rotation

--- view_rotation.py
+++ view_rotation.py
@@ -30,9 +30,9 @@ ax.scatter(X_axis[:,0], X_axis[:,1], X_axis[:,2], c='r', marker='.', s=0.7)
 ax.scatter(Y_axis[:,0], Y_axis[:,1], Y_axis[:,2], c='g', marker='.', s=0.7)
 ax.scatter(Z_axis[:,0], Z_axis[:,1], Z_axis[:,2], c='b', marker='.', s=0.7)
 
-ax.scatter(X[:,0], X[:,1], X[:,2], c='r', marker='.', label='X-axis')
-ax.scatter(Y[:,0], Y[:,1], Y[:,2], c='g', marker='.', label='Y-axis')
-ax.scatter(Z[:,0], Z[:,1], Z[:,2], c='b', marker='.', label='Z-axis')
+# ax.scatter(X[:,0], X[:,1], X[:,2], c='r', marker='.', label='X-axis', s=0.7)
+# ax.scatter(Y[:,0], Y[:,1], Y[:,2], c='g', marker='.', label='Y-axis', s=0.7)
+# ax.scatter(Z[:,0], Z[:,1], Z[:,2], c='b', marker='.', label='Z-axis', s=0.7)
 ax.legend()
 ax.set_xlabel('X-axis')
 ax.set_ylabel('Y-axis')
@@ -41,11 +41,11 @@ ax.set_title('3D Scatter Plot')
 ax.set_xlim(xmin=-1.0, xmax=1.0)
 ax.set_ylim(ymin=-1.0, ymax=1.0)
 ax.set_zlim(zmin=-1.0, zmax=1.0)
-ax.view_init(elev=90, azim=-90  )  # adjust these values as you like
+ax.view_init(elev=30, azim=-75  )  # adjust these values as you like
 
 
-omega = 0 *np.pi/180  # rotation angle in radians
-chi = 0*np.pi/180  # rotation angle in radians
+omega = 45 *np.pi/180  # rotation angle in radians
+chi = 45*np.pi/180  # rotation angle in radians
 phi = 45*np.pi/180  # rotation angle in radians
 
 # Create rotation matrices for each axis
@@ -69,9 +69,9 @@ X_rotated = np.dot(X, Rotation)
 Y_rotated = np.dot(Y, Rotation)
 Z_rotated = np.dot(Z, Rotation)
 
-ax.scatter(X_rotated[:,0], X_rotated[:,1], X_rotated[:,2], c='r', marker='.', s=0.5, label='X-axis rotated')
-ax.scatter(Y_rotated[:,0], Y_rotated[:,1], Y_rotated[:,2], c='g', marker='.', s=0.5, label='Y-axis rotated')
-ax.scatter(Z_rotated[:,0], Z_rotated[:,1], Z_rotated[:,2], c='b', marker='.', s=0.5, label='Z-axis rotated')
+ax.scatter(X_rotated[:,0], X_rotated[:,1], X_rotated[:,2], c='r', marker='*', s=5, label='X-axis rotated')
+ax.scatter(Y_rotated[:,0], Y_rotated[:,1], Y_rotated[:,2], c='g', marker='*', s=5, label='Y-axis rotated')
+ax.scatter(Z_rotated[:,0], Z_rotated[:,1], Z_rotated[:,2], c='b', marker='*', s=5, label='Z-axis rotated')
 
 
 plt.savefig('X-axis.png', dpi=300, bbox_inches='tight')
